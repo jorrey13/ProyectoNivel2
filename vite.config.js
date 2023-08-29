771a0ac11ee1c58f59a2cd58cf825a3234e6191f
jor_rey13 <jr.reyes2014@gmail.com>
2023-08-29 09:05:11 -0600

    añadiendo base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://jorrey13.github.io/ProyectoNivel2.2"
+  base:"https://jorrey13.github.io/ProyectoNivel2"
 })
